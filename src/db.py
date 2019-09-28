b29033afe621bdac2ad3895c666d31308e94e2b6
nima daryabar <nima.daryabar@yahoo.com>
2019-09-28 11:30:01 +0330

    src/db.py: showing all available databases

--- src/db.py
+++ src/db.py
@@ -4,6 +4,14 @@ from pymongo import MongoClient
 # Running Mongod instance
 client = MongoClient('localhost', 27017)
 
+# Show all available databases
+print("databases list:\n")
+db_list = client.list_database_names()
+for dbname in db_list:
+    print(dbname)
+
+# db_name = input('Give database name: ')
+
 # Create database
-db = client['reg_info_db']
+# db = client['reg_info_db']
 
