0b95610a8dfbaf0460566fd3342dbabc294c55b3
nima daryabar <nima.daryabar@yahoo.com>
2019-09-28 11:38:38 +0330

    src/db.py: adding enumerate() to print list

--- src/db.py
+++ src/db.py
@@ -7,8 +7,11 @@ client = MongoClient('localhost', 27017)
 # Show all available databases
 print("databases list:\n")
 db_list = client.list_database_names()
-for dbname in db_list:
-    print(dbname)
+for counter, dbname in enumerate(db_list, 1):
+    print(counter, '.', dbname)
+
+# print()
+# print(db_list[1])
 
 # db_name = input('Give database name: ')
 
