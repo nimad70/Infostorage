468ab360b2dbb433604ad75353e6bd38faf4dd1f
nima daryabar <nima.daryabar@yahoo.com>
2019-09-28 11:57:54 +0330

    src/db.py: if/else to make db or use from list

--- src/db.py
+++ src/db.py
@@ -1,3 +1,4 @@
+# Making a database using pymongo
 from pymongo import MongoClient
 
 
@@ -10,6 +11,19 @@ db_list = client.list_database_names()
 for counter, dbname in enumerate(db_list, 1):
     print(counter, '.', dbname)
 
+# Asking user to make a new db or used one from the list 
+check_answer_to_make_new_db = True
+while check_answer_to_make_new_db:
+    print()
+    answer_new_or_list = input("Making new datbase(n) or Using one from list(l) - (n/l): ")
+    if answer_new_or_list == 'n':
+        print(answer_new_or_list)
+        break
+    elif answer_new_or_list == 'l':
+        print(answer_new_or_list)
+        break
+    else:
+        print("\n*Wrong answer, Enter again plz!")
 # print()
 # print(db_list[1])
 
