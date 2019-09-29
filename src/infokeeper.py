4036a80004147fb27d457d4e96123f6fbd0ac870
nima daryabar <nima.daryabar@yahoo.com>
2019-09-29 10:15:08 +0330

    src/infokeeper.py: adding some comments

--- src/infokeeper.py
+++ src/infokeeper.py
@@ -3,6 +3,7 @@
 from db import connect_db
 
 
+# connect to database or create one
 db, new_database = connect_db()
 print("\n", db, "\n", new_database)
 
@@ -18,12 +19,15 @@ while check_retrieve_import:
         if new_database:
             print("You've just created a new database, "
                 "first you should import some data into database!")
+        # If there is data in database
         else:
             print(answer_retrieve_import)
 
+    # Import data into database
     elif answer_retrieve_import == 'i':
         print(answer_retrieve_import)
         pass
-
+    
+    # Wrong answer to retrieve(r) or import(i)
     else:
         print("\n*Wrong answer, Enter again plz!")
