106a9e790eb177b9629fe9a104ab1e3a973d5b42
nima daryabar <nima.daryabar@yahoo.com>
2019-09-29 10:11:30 +0330

    src/infokeeper.py: check for retrieve or import data from/into db is added

--- src/infokeeper.py
+++ src/infokeeper.py
@@ -3,5 +3,27 @@
 from db import connect_db
 
 
-db = connect_db()
-print(db)
+db, new_database = connect_db()
+print("\n", db, "\n", new_database)
+
+# Check if user enters the correct letter for retrieving or importing data from/into database
+check_retrieve_import = True
+while check_retrieve_import:
+    answer_retrieve_import = input("\nRetrieve(r) data from database or"
+        "Import(i) new data into the database(r/i): ")
+
+    # Retrieving data from database
+    if answer_retrieve_import == 'r':
+        # Check if user has just created a new database and db is empty
+        if new_database:
+            print("You've just created a new database, "
+                "first you should import some data into database!")
+        else:
+            print(answer_retrieve_import)
+
+    elif answer_retrieve_import == 'i':
+        print(answer_retrieve_import)
+        pass
+
+    else:
+        print("\n*Wrong answer, Enter again plz!")
