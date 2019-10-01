42800bf16002b9f43729187b9c53094c871506a7
nima daryabar <nima.daryabar@yahoo.com>
2019-10-01 17:33:22 +0330

    src/infokeeper.py: connect_db() and retrieve section are commented

--- src/infokeeper.py
+++ src/infokeeper.py
@@ -1,12 +1,15 @@
 # Author: Nima Daryabar aka nmdr
 # Keeping usernames and password
 from db import connect_db
+import datetime
 # from collections import *
 
 
 # connect to database or create one
+"""
 db, new_database = connect_db()
 print("\n", db, "\n", new_database)
+"""
 
 # Check if user enters the correct letter for retrieving or importing data from/into database
 check_retrieve_import = True
@@ -16,6 +19,8 @@ while check_retrieve_import:
 
     # Retrieving data from database
     if answer_retrieve_import == 'r':
+       pass
+       """
         # Check if user has just created a new database and db is empty
         if new_database:
             print("You've just created a new database, "
@@ -33,10 +38,24 @@ while check_retrieve_import:
 
 
             check_retrieve_import = False
+        """
 
     # Import data into database
     elif answer_retrieve_import == 'i':
         print(answer_retrieve_import)
+
+        
+        # Website or application name
+        web_app_name = input("Enter website or application's name: ")
+        username_ = input("Enter username: ")
+        password_ = input("Enter password")
+
+
+
+        # get username
+        # get password
+        # get web name
+        # get user comments
 
 
 
