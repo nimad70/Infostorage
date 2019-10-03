94307a6cbe2a0017fd688c6001f2cd9b08ad9b4c
nima daryabar <nima.daryabar@yahoo.com>
2019-10-03 10:30:17 +0330

    src/infokeeper.py: adding some changes to get_info() about validation and starting validate_info()

--- src/infokeeper.py
+++ src/infokeeper.py
@@ -12,11 +12,22 @@ def get_info(item_count):
     print(item_count)
     account_info = []
 
+    info_sympol = ""
+    is_validated = False
+
     counter_ = 0
     while counter_ != item_count:
         print("\nEnter info num.", counter_+1)
         # Get info from user about his account
-        app_name = input("\nEnter website or application's name: ")
+
+        while not is_validated:
+            app_name = input("\nEnter website or application's name: ")
+            info_sympol = 'app'
+            if validate_info(app_name, info_sympol):
+                is_validated = True
+            else:
+                print("Plz try again!\n")
+            
         username_ = input("Enter username: ")
         password_ = input("Enter password: ")
         comments = ""
@@ -26,16 +37,16 @@ def get_info(item_count):
         while comments_check_answer:
             
             # Check if user has any comments about his info
-            is_comments = input("Have any comments(y/n)? ")
+            has_comments = input("Have any comments(y/n)? ")
 
             # Yes
-            if is_comments == 'y':
+            if has_comments == 'y':
                 comments = input("Enter Your comments:  ")
                 comments_check_answer = False
                 break
             
             # No
-            elif is_comments == 'n':
+            elif has_comments == 'n':
                 print("Good!")
                 comments_check_answer = False
                 break
@@ -50,11 +61,27 @@ def get_info(item_count):
     return account_info
 
 
-# Validating user's account info
-def validate_info(web_name, user_name, pass_word, comments):
-    # validate username, ... by their sizes and other things
-    # return validated data
-    pass
+""" Validating user's account info
+    Getting account info as value(username or password or ...) 
+    and info Symbol tp check which one is which
+    and return True/False as it's validate data or not
+"""
+def validate_info(val, symb):
+    is_valid = False
+
+    if symb == 'app':
+        pass
+    elif symb == 'usnm':
+        pass
+    elif symb == 'pass':
+        pass
+    elif symb == 'cmnt':
+        pass
+    else:
+        print("Wrong symbol! and that's not ypur fault!")
+
+    return is_valid
+
 
 # connect to database or create one
 """
