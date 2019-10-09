b7370401f3b7703762ae0927760b11a877e056e1
nima daryabar <nima.daryabar@yahoo.com>
2019-10-09 09:32:42 +0330

    src/db.py: create_db() could make a new db!

--- src/db.py
+++ src/db.py
@@ -9,86 +9,6 @@ import re
 
 
 
-
-
-
-# Connecting to the database
-def connect_db():
-    # Running Mongod instance
-    client = MongoClient('localhost', 27017)
-
-    database_list = db_list(client)
-
-    # Asking user to make a new db or used one from the list
-    new_db = False
-
-    if not database_list:
-        print("\nCreate a database to continue")
-        create_db()
-    else:
-        pass
-
-
-
-
-    check_answer_to_make_new_db = True
-    while check_answer_to_make_new_db:
-        answer_new_or_list = input("\nMaking new datbase(n) or Using one from list(l) - (n/l): ")
-        
-        # Making a new databse
-        if answer_new_or_list == 'n':
-
-            # Check if name exists or not
-            db_name_exists = True
-            while(db_name_exists):
-                # Getting database name from user
-                db_name_user = input('\nGive a database name to create one: ')
-                print(db_name_user)
-
-                # if db name exists
-                if db_name_user in database_list:
-                    print("\n-> The database is already created! Enter New name plz!")
-                else:
-                    # Create database
-                    # db = client['reg_info_db']
-                    db = client[db_name_user]
-                    print("\nDatabase is created")
-                    db_name_exists = False
-                    new_db = True
-                    check_answer_to_make_new_db = False
-                    break
-            
-        
-        # Using db from old databases in the list
-        elif answer_new_or_list == 'l':
-            print(answer_new_or_list)
-            
-            # Check if user enters correct number
-            check_given_number = True
-            while check_given_number:
-                # Getting number from user
-                db_list_num = int(input("\nEnter database list number: "))
-                # Check if number is in the range of db_list length
-                if db_list_num in range((len(database_list)+1)):
-                    check_given_number = False
-                    break
-                else:
-                    print("Wrong number! plz enter number in a range of 1 to ", len(database_list))
-            
-            print(database_list[(db_list_num-1)])
-            choosen_db = database_list[(db_list_num-1)]
-            print("choosen:", choosen_db)
-            db = client[choosen_db]
-            check_answer_to_make_new_db = False
-            break
-
-        # Wrong answer to make db or use from the list question
-        else:
-            print("\n*Wrong answer, Enter again plz!")
-        
-    return db, new_db
-
-
 # Show databases list
 # pass 'client' from connect_db() to module as 'db_client'
 # list of databases(db_list) as return value
@@ -108,8 +28,90 @@ def db_list(db_client):
 
 
 # Create new datbase
-def create_db():
-    pass
+def create_db(dbls_list, dbls_client):
+    # Return True as a new db is created
+    is_new_db = True
+
+    # Check if name exists or not
+    db_name_exists = True
+    while(db_name_exists):
+        # Getting database name from user
+        db_name_user = input('\nGive a database name to create one: ')
+        print(db_name_user)
+
+        # if db name exists
+        if db_name_user in dbls_list:
+            print("\n-> The database is already created! Enter New name plz!")
+        else:
+            # Create database
+            # db = client['reg_info_db']
+            created_db = dbls_client[db_name_user]
+            print("\nDatabase is created")
+            db_name_exists = False
+    
+    return created_db, is_new_db
+
+
+
+# Connecting to the database
+def connect_db():
+    # Running Mongod instance
+    client = MongoClient('localhost', 27017)
+
+    # Show databases list
+    database_list = db_list(client)
+
+    # Turn to True if user makes a new database
+    new_db = False
+
+    # Check if there is any db in db_list to make new database or not
+    if not database_list:
+        print("\nCreate a database to continue")
+        db, new_db = create_db(database_list, client)
+    else:
+        check_answer_to_make_new_db = True
+        while check_answer_to_make_new_db:
+            answer_new_or_list = input("\nMaking new datbase(n) or Using one from list(l) - (n/l): ")
+            
+            # Making a new databse
+            if answer_new_or_list == 'n':
+                # Create new database
+                db, new_db = create_db(database_list, client)
+                print(db, "\n", new_db)
+                
+                check_answer_to_make_new_db = False
+                break
+                
+            # Using db from old databases in the list
+            elif answer_new_or_list == 'l':
+                pass
+                # print(answer_new_or_list)
+                
+                # # Check if user enters correct number
+                # check_given_number = True
+                # while check_given_number:
+                #     # Getting number from user
+                #     db_list_num = int(input("\nEnter database list number: "))
+                #     # Check if number is in the range of db_list length
+                #     if db_list_num in range((len(database_list)+1)):
+                #         check_given_number = False
+                #         break
+                #     else:
+                #         print("Wrong number! plz enter number in a range of 1 to ", len(database_list))
+                
+                # print(database_list[(db_list_num-1)])
+                # choosen_db = database_list[(db_list_num-1)]
+                # print("choosen:", choosen_db)
+                # db = client[choosen_db]
+                # check_answer_to_make_new_db = False
+                # break
+
+            # Wrong answer to make db or use from the list question
+            else:
+                print("\n*Wrong answer, Enter again plz!")
+        
+    return db, new_db
+
 
 
 """
