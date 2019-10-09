3f3d0660360081167571a17bd94e8e0eb153e5e1
nima daryabar <nima.daryabar@yahoo.com>
2019-10-09 14:52:29 +0330

    src/regcheck.py: check_db_name()'s code from db.py is moved to check_only_letters()

---
+++ src/regcheck.py
@@ -0,0 +1,13 @@
+# Author: Nima Daryabar
+import re
+
+# Check names for being only letters
+# True False as return value
+def check_only_letters(name):
+    is_name = True
+    # only letters regex
+    name_reg = re.compile('^([a-zA-Z]+)$')
+    if not re.match(name_reg, name):
+        is_name = False
+    
+    return is_name
