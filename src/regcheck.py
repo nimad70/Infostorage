2caa0b0de5e5b982088ac7f54702efefd62b423b
nima daryabar <nima.daryabar@yahoo.com>
2019-10-09 15:57:01 +0330

    src/regcheck.py: check_only_digit() is added

--- src/regcheck.py
+++ src/regcheck.py
@@ -12,3 +12,15 @@ def check_only_letters(name):
         is_name = False
     
     return is_name
+
+
+# Check numbers for being only digits
+# True False as return value
+def check_only_digits(numb):
+    is_digit = True
+    # only letters regex
+    digit_reg = re.compile('^([\d]+)$')
+    if not re.match(digit_reg, numb):
+        is_digit = False
+    
+    return is_digit
