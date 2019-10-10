3fa9c4fcef152124594bc2e233632be81228c905
nima daryabar <nima.daryabar@yahoo.com>
2019-10-10 20:02:16 +0330

    src/db_collections.py: choose_collection() gets the number and return the right collection

--- src/db_collections.py
+++ src/db_collections.py
@@ -5,7 +5,7 @@ from regcheck import *
 
 
 # show a list of collections or make a new one
-# coll_list_db is db
+# coll_list_db: db from insert_data()
 def collection_list(coll_list_db):
     print("\n List of collections:")
     collectn_list = coll_list_db.list_collection_names()
@@ -20,8 +20,8 @@ def collection_list(coll_list_db):
 
 
 # create a collection
-# create_coll_db is db
-# collec_list from collection_list() returned value
+# create_coll_db: db from insert_data()
+# collec_list: collection list from collection_list()
 def create_collection(craete_coll_db, collec_list):
     # Check if name exists or not
     coll_name_exists = True
@@ -45,27 +45,32 @@ def create_collection(craete_coll_db, collec_list):
 
 
 # choose collection from list
-# choose_coll_db is db
-# choose_coll_list from collection_list() returned value
+# choose_coll_db: db from insert_data()
+# choose_coll_list: collection list from collection_list()
 def choose_collection(choose_coll_db, choose_coll_list):
     # Check if user enters correct number
     check_given_number = True
     while check_given_number:
         # Getting number from user
         print("\nEnter collection list number: ", end=' ')
-        coll_list_num = 
+        coll_list_num = digit_check()
         print("leeeeeenn: ",len(choose_coll_list)," num: ", coll_list_num)
+
         # Check if number is in the range of db_list length
-        if coll_list_num in range(len(choose_coll_list)):
+        if coll_list_num == 0:
+            print("Wrong number! again plz!")
+            continue
+        if coll_list_num in range(len(choose_coll_list)+1):
             check_given_number = False
             break
         else:
             print("Wrong number! plz enter number in a range of 1 to ", len(choose_coll_list))
 
     print(choose_coll_list[(coll_list_num-1)])
-    choosen_coll = choose_coll_list[(coll_list_num-1)]
+    choosen_coll = choose_coll_list[(coll_list_num-1)]  # Get collection name from collection list
     print("choosen:", choosen_coll)
-    ch_coll = choose_coll_db[choosen_coll]
+    ch_coll = choose_coll_db[choosen_coll] # Get collection from database
+    print("ch_coll: ", ch_coll)
 
     return ch_coll
 
