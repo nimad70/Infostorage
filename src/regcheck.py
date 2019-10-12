6281a84692c472fb0537c242a091eaf48088b15f
nima daryabar <nima.daryabar@yahoo.com>
2019-10-12 18:04:50 +0330

    src/regcheck.py: use while 'True': instead of while boolean_variabale in digit_check() and reform the codes

--- src/regcheck.py
+++ src/regcheck.py
@@ -6,11 +6,9 @@ import re
 # True False as return value
 def check_only_letters(name):
     is_name = True
-    # only letters regex
-    name_pattern = re.compile(r"^([a-zA-Z]+)$")
+    name_pattern = re.compile(r"^([a-zA-Z]+)$") # only letters regex
     if not re.match(name_pattern, name):
         is_name = False
-    
     return is_name
 
 
@@ -18,24 +16,19 @@ def check_only_letters(name):
 # True False as return value
 def check_only_digits(numb):
     is_digit = True
-    # only letters regex
-    digit_pattern = re.compile(r"^([\d]+)$")
+    digit_pattern = re.compile(r"^([\d]+)$") # only letters regex
     if not re.match(digit_pattern, numb):
         is_digit = False
-    
     return is_digit
 
 
 # Check and get user's given number
 def digit_check():
-    # Check if number is only digits
-    digit_check = True
-    while digit_check:
+    while True: # Check if number is only digits
         try:
-            # Get number from user
-            num = int(input())
-        except ValueError:
+            num = int(input()) # Get number from user
+        except ValueError: # if num is not digit
             print("-> Wrong! Enter a number please:", end=' ')
-        else:
-            digit_check = False
+        else: # num is digit
+            break
     return num
