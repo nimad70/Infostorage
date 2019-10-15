e71c01bf6c97c1a3707248c07db41b77b3113d45
nima daryabar <nima.daryabar@yahoo.com>
2019-10-15 19:20:53 +0330

    setup.py: a comment is added tp module

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# use 'pip install -e .' to install packages due to top-level error in rood directory
 from setuptools import setup, find_packages
 
 setup(name='src', version='1.0', packages=find_packages())
